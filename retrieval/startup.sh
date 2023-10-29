39a3d4b201d62a443e1d3660981c11666a137fea
Guilherme Almeida <gui1612@sapo.pt>
2023-10-29 18:24:58 +0000

    feat: adds inital solr schema

---
+++ retrieval/startup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+usage() {
+    echo "Usage: $0 [-s <schema_file>] [-d <data_file>]"
+    echo "Optional: -s <schema_file> Specify the schema file (default: schema1.json)"
+    echo "          -d <data_file> Specify the data file (default: backup-tracks.json)"
+    echo "          -c <core_name> Specify the name for the core initially created (default: tracks)"
+    exit 1
+}
+
+schema_file="schema1.json"
+data_file="backup-tracks.json"
+core_name="tracks"
+
+while getopts "s:d:c:" opt; do
+    case $opt in
+        s) schema_file=$OPTARG;;
+        d) data_file=$OPTARG;;
+        c) core_name=$OPTARG;;
+        *) usage;;
+    esac
+done
+
+precreate-core tracks
+
+# Start Solr in background mode so we can use the API to upload the schema
+solr start
+
+# Schema definition via API
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary @"./$schema_file" \
+    "http://localhost:8983/solr/$core_name/schema"
+
+# Populate collection
+bin/post -c $core_name /data/"$data_file"
+
+# Restart in foreground mode so we can access the interface
+solr restart -f
